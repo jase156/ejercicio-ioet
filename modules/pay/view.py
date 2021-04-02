17e7545cd7130a9b7b1d499021dbf2597c06024d
Javier Sarango <javier_sarango@hotmail.com>
2021-04-02 15:50:04 -0500

    Configuración de view, vistas iniciales

--- modules/pay/view.py
+++ modules/pay/view.py
@@ -1,6 +1,53 @@
 class ViewPay(object):
+    
     @staticmethod
     def show_main_menu():
         print("///////////////////////////////")
-        print("// Gestor de pago //")
-        print("// ACME //")
+        print("//       Gestor de pago      //")
+        print("//            ACME           //")
+        print("//                           //")
+        print("//  1.-Calculo de pago       //")
+        print("//  2.-Salir                 //")
+        print("//                           //")
+        print("///////////////////////////////")
+        
+        #Pedir entrada de texto
+        print('Opción',end=": ")
+        return input()
+    
+    @staticmethod
+    def show_set_dataset_menu():
+        print("///////////////////////////////////")
+        print("//         Gestor de pago        //")
+        print("//              ACME             //")
+        print("//                               //")
+        print("//  Opción 1 : Calculo de pago   //")
+        print("//                               //")
+        print("//  1.-Ingresar ruta de archivo  //")
+        print("//  2.-Regresar                  //")
+        print("//                               //")
+        print("///////////////////////////////////")
+        
+        #Pedir entrada de texto
+        print('Opción',end=": ")
+        return input()
+
+    @staticmethod
+    def show_calculate_pay_menu():
+        print("///////////////////////////////////")
+        print("//         Gestor de pago        //")
+        print("//              ACME             //")
+        print("//                               //")
+        print("//  Opción 1 : Ingresar ruta de  //")
+        print("//             archivo           //")
+        print("//                               //")
+        print("//  1.-Calculo total de          //")
+        print("//     empleados                 //")
+        print("//  2.-Calculo por empleado      //")
+        print("//  3.-Regresar                  //")
+        print("//                               //")
+        print("///////////////////////////////////")
+        
+        #Pedir entrada de texto
+        print('Opción',end=": ")
+        return input()
