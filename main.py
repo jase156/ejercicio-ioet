994c45606a3bbb5e505e5c31a34ab99a296dc97a
Javier Sarango <javier_sarango@hotmail.com>
2021-04-04 19:14:37 -0500

    Modificación de main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from modules.pay import ModelPay
 from modules.pay import ViewPay
 from modules.pay import ControllerPay
+from modules.pay import Business
 
 if __name__ == "__main__":
-    run = ControllerPay(ModelPay(),ViewPay())
+    run = ControllerPay(ModelPay(),ViewPay(), Business())
     run.admin_main_menu()
     
