fc38f6f5c2e7c2fdd89d601e2efe1a42d443b681
Javier Sarango <javier_sarango@hotmail.com>
2021-04-04 19:14:24 -0500

    Componentes finales de controller

--- modules/pay/controller.py
+++ modules/pay/controller.py
@@ -2,14 +2,15 @@ from core import TypeFileException, NotFileException
 
 class ControllerPay(object):
     
-    def __init__(self, model, view):
+    def __init__(self, model, view, business):
         self.model = model
         self.view = view
+        self.business = business
         self.mesagge = "Ingres unicamente las opciones presentadas en pantalla"
     
     def admin_main_menu(self):
         while True:      
-            switch = {'1':self.calculate_pay_all,'2':exit}
+            switch = {'1':self.calculate_menu,'2':exit}
             case = self.view.show_main_menu()
             if case in switch:
                 switch[case]()
@@ -17,13 +18,13 @@ class ControllerPay(object):
                 self.view.show_message_error(self.mesagge)
     
     
-    def calculate_pay_all(self):
+    def calculate_menu(self):
         while True:
             switch = {'1':self.input_file,'2':self.breaks}
             case = self.view.show_set_dataset_menu()
             if case in switch:
-                if not switch[case]():
-                    return True
+                if switch[case]():
+                    return False
             else:
                 self.view.show_message_error(self.mesagge)
         
@@ -33,16 +34,57 @@ class ControllerPay(object):
             try:
                 file_path = self.view.show_input_file()
                 if file_path in switch:
-                    return True   
-                data_set = self.model.read_schedule(file_path)
+                    return False   
+                self.data_set = self.model.read_schedule(file_path)
+                if not self.data_set:
+                    self.view.show_message_error("El documento ingresado se encuentra vacio, intente nuevamente con uno nuevo")
+                else: 
+                    self.select_calculate(file_path)
             except NotFileException as e:
                 self.view.show_message_error(e)
             except TypeFileException as e:
                 self.view.show_message_error(e)
-            
     
+    def select_calculate(self, file):
+        while True:
+            switch = {'1':self.calculate_pay_all,'2':self.calculate_pay_people,'3':self.breaks}
+            case = self.view.show_calculate_pay_menu(file)
+            if case in switch:
+                if switch[case]():
+                    return False
+            else:
+                self.view.show_message_error(self.mesagge)
+            
+            
+    def calculate_pay_all(self):
+        while True:
+            switch = {'1':self.breaks}
+            payments = self.business.payments(self.data_set)
+            case = self.view.show_pay_all(payments)
+            if case in switch:
+                if switch[case]():
+                    return False
+            else:
+                self.view.show_message_error(self.mesagge)
+                
+        
+    def calculate_pay_people(self):
+        while True:
+            name = self.view.input_name()
+            if name in self.data_set:
+                switch = {'1':self.breaks}
+                payments = self.business.payments({name:self.data_set[name]})
+                case = self.view.show_pay_all(payments)
+                if case in switch:
+                    if switch[case]():
+                        return False
+                else:
+                    self.view.show_message_error(self.mesagge)
+            else:
+                self.view.show_message_error("El nombre no se encuentra en el registro")
+
         
     def breaks(self):
-        return False
+        return True
         
         
